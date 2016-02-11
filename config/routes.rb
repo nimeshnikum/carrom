c4a186b66dc6d85a6b5882b7d54ddeb021bf0ae9
Johnson Arokiaraj <johnsonarokiaraj@gmail.com>
2016-02-11 12:11:18 +0530

    Add rules

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
 
   get 'show_player', to: 'home#show_player', as: 'show_player'
   put 'assign_player/:id', to: 'home#assign_player', as: 'assign_player'
+  get 'rules', to: 'home#rules', as: 'rules'
 end
