4888c0a8df09a675c8b4f7ab0e2713bd59fb846b
Johnson Arokiaraj <johnsonarokiaraj@gmail.com>
2016-02-23 03:59:35 +0530

    Add match to decs in match controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,7 +4,7 @@ class MatchesController < ApplicationController
   # GET /matches
   # GET /matches.json
   def index
-    @matches = Match.all
+    @matches = Match.order(match_no: :desc)
   end
 
   # GET /matches/new
