7d7e8c529a5cf543e7d3c9212b4f130a22c72329
Johnson Arokiaraj <johnsonarokiaraj@gmail.com>
2016-02-23 05:33:39 +0530

    update Result pages

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -45,7 +45,7 @@ class MatchesController < ApplicationController
   # PATCH/PUT /matches/1.json
   def update
     respond_to do |format|
-      if @match.update(match_params)
+      if @match.update_attributes(:winning_team_id => params["match"]["winning_team"], :queen_player_id => params["match"]["queen_player"])
         format.html { redirect_to matches_path, notice: 'Match was successfully updated.' }
         format.json { head :no_content }
       else
--- app/models/match.rb
+++ app/models/match.rb
@@ -4,8 +4,13 @@ class Match < ActiveRecord::Base
 
   belongs_to :team1, :class_name => 'Team', :foreign_key => 'team1_id'
   belongs_to :team2, :class_name => 'Team', :foreign_key => 'team2_id'  
+  belongs_to :winning_team, :class_name => 'Team', :foreign_key => 'winning_team_id'  
 
-  attr_accessor :player1, :player2, :player3, :player4
+  attr_accessor :player1, :player2, :player3, :player4, :queen_player
+
+  def teams
+    [self.team1 , self.team2]
+  end
 
   def player_1
     player = self.match_players[0].player
@@ -26,4 +31,9 @@ class Match < ActiveRecord::Base
     player = self.match_players[3].player
     player if player.team == self.team2
   end
+
+  def queen_holder
+    Player.find(self.queen_player_id)
+  end
+
 end
