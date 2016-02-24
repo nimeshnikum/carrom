b224502bafbef0bf18ba57be0bdedce97926d2ab
Johnson Arokiaraj <johnsonarokiaraj@gmail.com>
2016-02-24 16:43:45 +0530

    Fix bug for results

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ActiveRecord::Base
   has_many :players
-
+  has_many :winning_matches, :class_name => 'Match', :foreign_key => 'winning_team_id'
   def total_amount
     self.score - (self.players.map { |x| x.score }.sum)
   end
@@ -9,5 +9,9 @@ class Team < ActiveRecord::Base
     match_points = Match.where(winning_team_id: self.id).count*15
     queen_points = Match.joins("inner join players on players.id = matches.queen_player_id and players.team_id = #{self.id}").count*5
     match_points + queen_points
+  end
+
+  def board_points
+    self.winning_matches.map { |x| x.board_points }.sum
   end
 end
