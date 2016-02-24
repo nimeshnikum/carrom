4d87f2ed810207e9a9936a8dce02d6e37ba21e9d
Johnson Arokiaraj <johnsonarokiaraj@gmail.com>
2016-02-24 18:05:28 +0530

    Add queen count to results

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,6 @@
 class Player < ActiveRecord::Base
   belongs_to :team
-
+  has_many :queens, :class_name => 'Match', :foreign_key => 'queen_player_id'
   has_many :matches,  :through => :match_players
   has_many :match_players, dependent: :destroy
 
@@ -10,5 +10,29 @@ class Player < ActiveRecord::Base
 
   def self.get_random_player
     Player.where("team_id" => nil).shuffle.first
+  end
+
+  def matches_count
+    self.queens.count
+  end
+
+  def queen_count
+    self.queens.count
+  end
+
+  def coin_count
+    0
+  end
+
+  def op_coin_count 
+    0
+  end
+
+  def due_count
+    0
+  end
+
+  def player_score
+    0
   end
 end
